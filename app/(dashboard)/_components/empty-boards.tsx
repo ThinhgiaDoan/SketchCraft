3214f1aceda3b882a21942b326f7845a2d57c85a
ThinhgiaDoan <hopee.giathinh@gmail.com>
2024-02-04 14:35:09 +0700

    create mutation

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,8 +1,29 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import { api } from "@/convex/_generated/api";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { useOrganization } from "@clerk/nextjs";
 import Image from "next/image";
-import React from "react";
 
 function EmptyBoards() {
+  const { organization } = useOrganization();
+  const { mutate, pending } = useApiMutation(api.board.create);
+
+  const onClick = () => {
+    if (!organization) return;
+
+    mutate({
+      orgId: organization.id,
+      title: "Untitled",
+    })
+      .then((id) => {
+        toast.success("Board created");
+      })
+      .catch(() => toast.error("Failed to create board"));
+  };
+
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <Image src="/note.svg" height={110} width={110} alt="Empty favorites" />
@@ -10,7 +31,9 @@ function EmptyBoards() {
       <h2 className="text-2xl font-semibold mt-6">Create your first board!</h2>
       <p>Start by creating a board for your organization</p>
       <div className="mt-6">
-        <Button size={"lg"}>Create board</Button>
+        <Button disabled={pending} size={"lg"} onClick={onClick}>
+          Create board
+        </Button>
       </div>
     </div>
   );
