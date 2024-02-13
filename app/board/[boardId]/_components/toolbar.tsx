3c5a594acf5562fa4be928fa4d6fa0e177acaf42
ThinhgiaDoan <hopee.giathinh@gmail.com>
2024-02-13 23:07:42 +0700

    config liveblocks

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function Toolbar() {
   return (
     <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
@@ -19,3 +17,9 @@ function Toolbar() {
 }
 
 export default Toolbar;
+
+Toolbar.Skeleton = function ToolbarSkeleton() {
+  return (
+    <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 bg-white h-[360px] w-[52px] shadow-md rounded-md"></div>
+  );
+};
--- components/room.tsx
+++ components/room.tsx
@@ -4,16 +4,16 @@ import { ClientSideSuspense } from "@liveblocks/react";
 import { RoomProvider } from "@/liveblocks.config";
 import { ReactNode } from "react";
 
-export const Room = ({
-  children,
-  roomId,
-}: {
+interface RoomProps {
   children: ReactNode;
   roomId: string;
-}) => {
+  fallback: NonNullable<ReactNode> | null;
+}
+
+export const Room = ({ children, roomId, fallback }: RoomProps) => {
   return (
     <RoomProvider id={roomId} initialPresence={{}}>
-      <ClientSideSuspense fallback={<div>Loading...</div>}>
+      <ClientSideSuspense fallback={fallback}>
         {() => children}
       </ClientSideSuspense>
     </RoomProvider>
