02e5b084b9cc7887e2fda38e7c73a53f12706069
ThinhgiaDoan <hopee.giathinh@gmail.com>
2024-02-18 20:34:21 +0700

    room participant

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,7 +1,38 @@
+"use client";
+
+import { useOthers, useSelf } from "@/liveblocks.config";
+import { UserAvatar } from "./user-avatar";
+import { User } from "@liveblocks/client";
+
+const MAX_SHOWN_USERS = 2;
+
 function Participants() {
+  const users = useOthers();
+  const currentUser: any = useSelf();
+  const hasMoreUsers = users.length > MAX_SHOWN_USERS;
+
+  console.log("currentUser", currentUser);
+
   return (
     <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
-      List of users
+      {users.slice(0, MAX_SHOWN_USERS).map(({ connectionId, info }: any) => {
+        return (
+          <UserAvatar
+            key={connectionId}
+            src={info?.picture}
+            name={info?.name}
+            fallback={info?.name?.[0] || "A"}
+          />
+        );
+      })}
+
+      {currentUser && (
+        <UserAvatar
+          src={currentUser.info?.picture}
+          name={`${currentUser.info?.name} (You)`}
+          fallback={currentUser.info?.name?.[0]}
+        />
+      )}
     </div>
   );
 }
--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -2,9 +2,9 @@ import { createClient } from "@liveblocks/client";
 import { createRoomContext } from "@liveblocks/react";
 
 const client = createClient({
-  publicApiKey:
-    "pk_dev_CcahHnHbGDvBB4GUmuaAkMTmUgI0dsPojbS8JfvRr4thTe7eiSqaOYYTWXBQ7YrS",
-  // authEndpoint: "/api/liveblocks-auth",
+  // publicApiKey:
+  //   "pk_dev_CcahHnHbGDvBB4GUmuaAkMTmUgI0dsPojbS8JfvRr4thTe7eiSqaOYYTWXBQ7YrS",
+  authEndpoint: "/api/liveblocks-auth",
 });
 
 // Presence represents the properties that exist on every user in the Room
@@ -28,19 +28,16 @@ type Storage = {
 // provided by your own custom auth back end (if used). Useful for data that
 // will not change during a session, like a user's name or avatar.
 type UserMeta = {
-  // id?: string,  // Accessible through `user.id`
-  // info?: Json,  // Accessible through `user.info`
-};
-
-// Optionally, the type of custom events broadcast and listened to in this
-// room. Use a union for multiple events. Must be JSON-serializable.
-type RoomEvent = {
   id?: string;
   info?: {
     name?: string;
     picture?: string;
   };
 };
+
+// Optionally, the type of custom events broadcast and listened to in this
+// room. Use a union for multiple events. Must be JSON-serializable.
+type RoomEvent = {};
 
 // Optionally, when using Comments, ThreadMetadata represents metadata on
 // each thread. Can only contain booleans, strings, and numbers.
